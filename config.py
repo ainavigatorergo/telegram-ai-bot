2ce9c14740e2fee08119b9363e6c7c3c460696a4
ainavigatorergo <egoras21@mail.ru>
2026-09-10 15:06:41 +0300

    Add SELF_URL environment variable to config

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 POST_TIMES = os.getenv("POST_TIMES", "10:00,19:00").split(',')
 PORT = int(os.getenv("PORT", 8080))
+SELF_URL = os.getenv("SELF_URL", "https://ainavbot.onrender.com")  # URL на Render
 
 TOPICS_FILE = "used_topics.json"
 HISTORY_FILE = "post_history.json"
